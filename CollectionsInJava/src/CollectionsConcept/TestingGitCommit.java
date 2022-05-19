d4837175b0ec6200fc05913bd07c8a44e9b56957
admin <admin@DESKTOP-734T7OV>
2022-05-19 16:24:40 +0530

    Adding a new class "TestingGitCommit

---
+++ CollectionsInJava/src/CollectionsConcept/TestingGitCommit.java
@@ -0,0 +1,11 @@
+package CollectionsConcept;
+
+public class TestingGitCommit {
+
+	public static void main(String[] args) {
+
+			System.out.println("I am happy to make this new commit");
+
+	}
+
+}
